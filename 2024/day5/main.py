20bf1ba9dcc5c1e5733dc2d8d3cbefabc929c0ed
Guillaume <guillaume.tanguy.wolf@gmail.com>
2025-01-30 13:21:54 +0100

    improvement in implementation

--- 2024/day5/main.py
+++ 2024/day5/main.py
@@ -4,6 +4,7 @@ import awkward as ak
 rules = []
 updates = []
 
+# Get inputs
 with open('input2.txt', 'r') as file:
     for line in file:
         split = line.strip().split('|')
@@ -11,7 +12,6 @@ with open('input2.txt', 'r') as file:
             rules.append([int(split[0]), int(split[1])])
         if len(split)==1 and not split==['']:
             updates.append([int(n) for n in split[0].split(',')])
-
 rules = ak.Array(rules)
 
 # print(rules[:5])
@@ -19,50 +19,55 @@ rules = ak.Array(rules)
 # print(*updates[:5], sep='\n')
 # print()
 
+
+
+# Part 1
 def correctly_ordered(update, rules):
-    validity = [True for _ in update]
-    for n in update:
-        for rule in rules[[n in r for r in rules]]:
-            nfirst = rule[0]==n
-            m = rule[1] if nfirst else rule[0]
-            if not m in update: continue
-            if not (update.index(n)<update.index(m))==nfirst:
-                validity[update.index(n)] = False
-                validity[update.index(m)] = False
-            # print(n, m, update.index(n), update.index(m), update.index(n)<update.index(m), nfirst)
-    # print(update)
-    # print(validity)
-    # print()
-    return ak.Array(validity)
+    valide_rule = np.ones(np.shape(rules)[0], dtype=bool)
+    for i, rule in enumerate(rules):
+        if not np.all([n in update for n in rule]):
+            continue 
+        ind = [np.where(update==n)[0][0] for n in rule]
+        if ind[1]<ind[0]:
+            valide_rule[i] = False
+    return valide_rule
 
 
-def correct(update, rules, validity):
-    error = len(validity[validity==False])
-    new_update = update.copy()
-    for n in update:
-        if validity[update.index(n)]: continue
-        new_update.remove(n)
-        new_update.insert(update.index(n)+1, n)
-        new_validity = correctly_ordered(new_update, rules)
-        new_error = len(new_validity[new_validity==False])
-        print(validity, new_validity)
-        print(error, new_error)
-
-    return new_update    
+def get_error(valide_rule):
+    if np.all(valide_rule):
+        return 0
+    count = 0
+    for vr in valide_rule:
+        count += 0 if vr else 1
+    return count
 
 
+
+def correct(update, rules, valide_rule):
+    unvalide_rules = rules[valide_rule==False]
+    n_errors = get_error(valide_rule)
+    n_errors_new = n_errors
+    print(f'Unvalide rules: {unvalide_rules}')
+    for ur in unvalide_rules:
+        while n_errors<=n_errors_new:
+            m=0
 
 
 sum = 0
 print(*rules, sep='\n')
 for update in updates:
     print('update = ', update)
-    validity = correctly_ordered(update, rules)
-    if not len(validity[validity==False])==0:
-        correct(update, rules, validity)
+    valide_rule = correctly_ordered(update, rules)
+    n_errors = get_error(valide_rule)
+    print(f'{n_errors} updates are not valide !')
+    # if not n_errors==0: 
+    #     correct(update, rules, valide_rule)
+
     sum += update[int((len(update)-1)/2)]
     print()
     print()
     print()
 
+
+print(sum)
 
