e8bf6295e8e693ec7424286417dc2150be0496ea
Guillaume <guillaume.tanguy.wolf@gmail.com>
2025-01-30 17:33:37 +0100

    now its working, but step 2 take a lot of time to run. maybe need improvement

--- 2024/day5/main.py
+++ 2024/day5/main.py
@@ -5,7 +5,7 @@ rules = []
 updates = []
 
 # Get inputs
-with open('input2.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     for line in file:
         split = line.strip().split('|')
         if len(split)==2:
@@ -42,32 +42,94 @@ def get_error(valide_rule):
     return count
 
 
+def get_unvalide_rules(update, rules):
+    valide_rule = correctly_ordered(update, rules)
+    return rules[valide_rule==False]
+    
 
-def correct(update, rules, valide_rule):
+def correct(update, rules):
+    initial_update = update.copy()
+    valide_rule = correctly_ordered(update, rules)
     unvalide_rules = rules[valide_rule==False]
+    # print(unvalide_rules)
     n_errors = get_error(valide_rule)
-    n_errors_new = n_errors
-    print(f'Unvalide rules: {unvalide_rules}')
-    for ur in unvalide_rules:
-        while n_errors<=n_errors_new:
-            m=0
+    # print(f'Unvalide rules: {unvalide_rules}')
+    k = 0
+    while get_error(correctly_ordered(update, rules))>0:
+        # print(f'{n_errors} rule{('s, are' if n_errors>1 else ' is')} not valide !', unvalide_rules)
+        ind = [np.where(update==n)[0][0] 
+               for n in unvalide_rules[k]]
+        # print(ind, unvalide_rules[k])
+        # Lowering
+        if not ind[0]==0:
+            # print('(Lowering)')
+            new_update = update.copy()
+            n_minus1 = new_update[ind[0]-1]
+            n = new_update[ind[0]]
+            new_update[ind[0]] = n_minus1
+            new_update[ind[0]-1] = n
+            new_n_error = get_error(correctly_ordered(new_update, rules)) 
+            if new_n_error<=n_errors:
+                # print('Previous update: ', update)
+                # print('New update: ', new_update)
+                update = new_update.copy()
+                n_errors = new_n_error
+        # Raising
+        if not ind[1]+1==len(update):
+            # print('(Raising)')
+            new_update = update.copy()
+            n_plus1 = new_update[ind[1]+1]
+            n = new_update[ind[1]]
+            new_update[ind[1]] = n_plus1
+            new_update[ind[1]+1] = n
+            new_n_error = get_error(correctly_ordered(new_update, rules)) 
+            if new_n_error<=n_errors:
+                # print('Previous update: ', update)
+                # print('New update: ', new_update)
+                update = new_update.copy()
+                n_errors = new_n_error
+        
+        # check for any changes
+        # print(f'{initial_update = } at the begening of the loop. ')
+        # print(f'{update = } at the end of the loop. ')
+        if update==initial_update:
+            # print('No change happened, changing rule')
+            k+=1
+        else: 
+            unvalide_rules = get_unvalide_rules(update, rules)
+            initial_update = update.copy()
+            k=0
+        # print('end of one loop')
+
+    print('Final update:', update, f',  {n_errors = }')
+    return update
 
 
-sum = 0
+
+
+sum = [0,0]
 print(*rules, sep='\n')
 for update in updates:
     print('update = ', update)
-    valide_rule = correctly_ordered(update, rules)
-    n_errors = get_error(valide_rule)
-    print(f'{n_errors} updates are not valide !')
-    # if not n_errors==0: 
-    #     correct(update, rules, valide_rule)
-
-    sum += update[int((len(update)-1)/2)]
+    n_errors = get_error(correctly_ordered(update, rules))
+    if n_errors>0: 
+        print(f'{n_errors} rule{('s are' if n_errors>1 else ' is')} not satisfied !')
+        update = correct(update, rules)
+        sum[1] += update[int((len(update)-1)/2)]
+    else: 
+        print('The update is valide !')
+        sum[0] += update[int((len(update)-1)/2)]
     print()
     print()
     print()
 
 
 print(sum)
+
+
+
+
+
+
+
 
