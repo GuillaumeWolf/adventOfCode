54fe8bc554e056145a2322d00b335513ac85374e
Guillaume <guillaume.tanguy.wolf@gmail.com>
2025-01-31 21:07:41 +0100

    renamed

--- 2024/day6/main.py
+++ 2024/day6/main.py
@@ -1,13 +1,34 @@
 import numpy as np
-import time 
+import time
+from rich.progress import Progress, BarColumn, TextColumn
+import awkward as ak
 
 
-next_coor = {
+dirs = ('^', 'v', '>', '<')
+dir_change = {
+        '^':'>',
+        'v':'<',
+        '>':'v',
+        '<':'^',
+    }
+step = {
     '^':np.array((-1,0)),
     'v':np.array((1,0)),
     '>':np.array((0,1)),
     '<':np.array((0,-1)),
 }
+
+
+def turn_right(dir):
+    return dir_change[dir]
+
+def move_forward(pos, dir):
+    # pos = pos + step[dir]
+    return pos + step[dir]
+
+def move_backward(pos, dir):
+    # pos = pos - step[dir]
+    return pos - step[dir]
 
 
 # inputs
@@ -16,139 +37,101 @@ with open('input.txt') as file:
     for line in file:
         map.append([char for char in line.strip()])
 
-map = np.array(map)
+border = np.array((len(map), len(map[0])))
+walls = np.array([[map[i][j]=='#' 
+        for j in range(border[1])]
+        for i in range(border[0])])
+guard_start = {}
+for i in range(border[0]):
+    for j in range(border[1]):
+        if map[i][j] in dirs:
+            guard_start['pos'] = np.array((i,j))
+            guard_start['dir'] = map[i][j]
+visited_box = np.array([[0 
+    for j in range(border[1])] 
+    for i in range(border[0])
+])
 
-
-# Functions
-
-def show(map):
-    print(' ', end='')
-    for _ in range(map.shape[1]):print('_', end='')
-    print()
-    for line in map:
-        print('|', end='')
-        for box in line:
-            if box=='.': print(' ', end='')
-            elif box in list(next_coor.keys()):
-                print('\033[31m', box, '\033[0m', sep='', end='')
-            else:print(box, end='')
-        print('|')
-    print(' ', end='')
-    for _ in range(map.shape[1]):print('_', end='')
-    print()
-
-
-def path_length(map):
-    return len(map[map=='X'])
-
-
-def get_dir(map): 
-    dir_poss = np.array(('^', '<', '>', 'v'))
-    poss = list(dir in map for dir in dir_poss)
-    dir = dir_poss[poss][0]
-    return dir
-
-
-def get_coordonnee(map):
-    dir = get_dir(map)
-    return np.array(np.concatenate(
-        np.where(map==dir)))
-
-
-def in_map_range(coor, map):
-    if (not np.all(coor>=0)
-        or not np.all(coor<map.shape)):
-        return False
-    return True
-
-
-def make_a_step(map):
-    coordonnee = get_coordonnee(map)
-    dir = get_dir(map)
-    next_dir = {
-        '^':'>',
-        'v':'<',
-        '>':'v',
-        '<':'^',
-    }
-    new_coordonnee = coordonnee + next_coor[dir]
-    if in_map_range(new_coordonnee, map):
-        # Change coordonne
-        if map[*new_coordonnee]=='#':
-            dir = next_dir[dir]
-            new_coordonnee = coordonnee + next_coor[dir]
-        map[*coordonnee] = 'X'
-        map[*new_coordonnee] = dir
-    return not in_map_range(new_coordonnee, map)
-
-
-def mult(*input):
-    a = 1
-    for x in input:
-        a *= x
-    return a
-
-
-def measure_time(func, funcname, *args, **kwargs):
-    t0 = time.time()
-    output = func(*args, **kwargs)
-    t1 = time.time()
-    print(f'func {funcname} took {(t1-t0)*1000:.1f} ms to run. ')
-    return output
-
-
-# Part 1: solve
-if False:
-    show(map)
-    step = 0
-    out = False
-    while not out:
-        # print(f'Step {step}')
-        map, out = make_a_step(map)
-        # show(map)
-        # print('\n\n\n')
-        step += 1 
-    show(map)
-    print(path_length(map)+1)
+# print(f'{border =}')
+# print(f'{walls =}')
+# print(f'{guard_start =}')
+# print(f'{visited_box =}')
 
 
 
 
-# Part 2: solve
-box_that_loop = 0
-print(map.shape)
-start = {'coor': get_coordonnee(map), 'dir': get_dir(map)}
-wall = [(i,j)
-        for i in range(map.shape[0])
-        for j in range(map.shape[1])
-        if map[i,j]]
-for i in range(map.shape[0]):
-    for j in range(map.shape[1]):
-        if (i,j) in wall: continue
-        position = start.copy()
-        print(i,j)
-        looped = False
-                
-        
-        t0 = time.time()
-        map[i,j] = '#'
-        out = False
-        step = 0
-        while not out and not looped:
-            # print(step)
-            out = make_a_step(map)
-            step += 1 
-            looped = step>(mult(*map.shape))
-        if looped: 
-            print('Loop found')
-            show(map)
-            box_that_loop += 1
-        map[i,j] = '.'
-        map[*get_coordonnee(map)] = '.'
-        map[*start['coor']] = start['dir']
-        t1 = time.time()
-        print(f'Loop took {(t1-t0)*1000:.1f} ms to run. ')
+def is_inside(pos, border=border):
+    return np.all((pos>=0,pos<border))
 
-print(box_that_loop)
+
+
+
+def make_path(guard, walls=walls, border=border, return_path=False):
+    visited_box = np.zeros(border)
+    visited_box[*guard['pos']] += 1
+    not_in_loop = True
+    while (is_inside(move_forward(guard['pos'], guard['dir'])) 
+           and not_in_loop):
+        # print(f'{guard['pos'] = }')
+        guard['pos'] = move_forward(guard['pos'], guard['dir']) 
+        while walls[*guard['pos']]:
+            guard['pos'] = move_backward(guard['pos'], guard['dir'])
+            guard['dir'] = turn_right(guard['dir'])
+            guard['pos'] = move_forward(guard['pos'], guard['dir']) 
+        visited_box[*guard['pos']] += 1
+        # print(f'{guard['pos'] = }')
+        # print(f'{visited_box = }')
+        if 5 in visited_box:
+            not_in_loop = False
+    # print(visited_box)
+    if return_path:
+        return np.count_nonzero(visited_box), visited_box
+    return not_in_loop
+
+
+
+# Part 1
+t0 = time.time()
+n_boxs, visited_box = make_path(guard_start.copy(), return_path=True)
+print(f'{n_boxs = }')
+print(f'{visited_box = }')
+t1 = time.time()
+print(f'Part 1 took {t1-t0:.4f} s')
+
+
+
+# Part 2
+t0 = time.time()
+with Progress(
+    TextColumn("[progress.description]{task.description}"),
+    BarColumn(),
+    TextColumn("{task.completed}/{task.total}"),
+    transient=True 
+    ) as progress:
+    
+    task = progress.add_task("[cyan]Processing...", total=n_boxs)
+    n_loop = 0
+    x,y = np.where(np.logical_not(visited_box==0))
+    for i, j in zip(x,y):
+        tloop0 = time.time()
+        progress.update(task, advance=1)
+        if np.array_equal(np.array([i, j]), guard_start['pos']): continue
+        walls[i,j] = True
+        not_in_loop = make_path(guard_start.copy(), walls=walls)
+        if not not_in_loop:
+            print('\033[1m', 'Was stucked in a loop. ', '\033[0m', sep='') 
+            n_loop += 1
+        walls[i,j] = False
+        tloop1 = time.time()
+        print(f'Loop ({i},{j}) took {tloop1-tloop0:.4f} s')
+
+    print(f'{n_loop = }')
+    with open('output2.txt', 'w') as output_file:
+        output_file.write(str(n_loop))
+
+
+t1 = time.time()
+print(f'Part2 took {t1-t0:.4f} s')
+
 
 
